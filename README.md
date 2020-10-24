5edd394d53ce9f7da39d847d94cef1f944fa8631
Ben Mizel <ben.mizel@gmail.com>
2020-10-24 15:06:56 -0400

    Added sqlite3

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
 # Foresight Project
 
 ## Notes
-Download zip
-Open zip
-git init new folder
-create new repo on github without README, LICENSE, or .gitignore
-push existing repo to github repo
+Download the zip file I've been sent
+Open the zip file to download its contents in a new folder
+Open the new folder in VS Code
+In the terminal, initialize the folder/directory as a git repository with `git init`
+Create a new repository on GitHub with the same name as the folder (don't create a README, LICENSE, or .gitignore)
+In the terminal, set up the remote repository and push the local repository to GitHub
 
-npx create-react-app client
+Create a package.json with `npm init`
+Create a React frontend in a client directory with the command `npx create-react-app client`
+Create a server directory
+Create a database directory
+
 project
 -client
 -server
 -database
 
-server.js
-proxy client server
-concurrently run both from project directory package.json
+Create an Express server in server/server.js running on port 5000
+Within client/package.json, proxy requests sent to the React server's localhost:3000 over to the Express server's localhost:5000
+Update project/package.json with the following scripts:
+  "client": "cd client && npm start",
+  "server": "nodemon server/server.js",
+  "dev": "concurrently --kill-others-on-fail \"npm run server\" \"npm run client\""
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const md5 = require('md5');
+const db = require('../database/database.js');
+
 const app = express();
 const port = 5000;
 app.use(bodyParser.json());
@@ -14,5 +17,89 @@ app.post('/api/world', (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
+
+// Root endpoint
+app.get("/", (req, res, next) => {
+  res.json({"message":"Ok"})
+});
+
+app.get("/api/users", (req, res, next) => {
+  var sql = "select * from user"
+  var params = []
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":rows
+      })
+    });
+});
+
+app.get("/api/user/:id", (req, res, next) => {
+  var sql = "select * from user where id = ?"
+  var params = [req.params.id]
+  db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":row
+      })
+    });
+});
+
+app.post("/api/user/", (req, res, next) => {
+  var errors=[]
+  if (!req.body.password){
+      errors.push("No password specified");
+  }
+  if (!req.body.email){
+      errors.push("No email specified");
+  }
+  if (errors.length){
+      console.log('ERRORS!!!!!')
+      res.status(400).json({"error":errors.join(",")});
+      return;
+  }
+  var data = {
+      name: req.body.name,
+      email: req.body.email,
+      password : md5(req.body.password)
+  }
+  var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
+  var params =[data.name, data.email, data.password]
+  db.run(sql, params, function (err, result) {
+      if (err){
+          res.status(400).json({"error": err.message})
+          return;
+      }
+      res.json({
+          "message": "success",
+          "data": data,
+          "id" : this.lastID
+      })
+  });
+})
+
+// Default response for any other request
+app.use(function(req, res){
+  res.status(404);
+});
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Unknown server error',
+    status: 500,
+    message: 'Check server logs for more info',
+  };
+  const newErr = Object.assign({}, defaultErr, err);
+  console.log('SERVER LOG: ', newErr.log);
+  res.status(newErr.status).send(newErr.message);
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
